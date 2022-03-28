852fcd04095a584bcd29b8d40a32d21062d60617
MicheleAdm <michele1adami96@gmail.com>
2022-03-28 23:16:15 +0200

    added methods for attachment download

--- src/main/java/it/be/codingRace/service/AttachmentService.java
+++ src/main/java/it/be/codingRace/service/AttachmentService.java
@@ -1,6 +1,8 @@
 package it.be.codingRace.service;
 
-import it.be.codingRace.exception.TicketException;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.stereotype.Service;
@@ -14,8 +16,6 @@ import it.be.codingRace.exception.AttachmentException;
 import it.be.codingRace.exception.AttachmentException.Type;
 import it.be.codingRace.repository.AttachmentRepository;
 import it.be.codingRace.utils.ValidatorUtils;
-
-import java.util.List;
 
 @Service
 @Transactional(rollbackFor = Exception.class, propagation = Propagation.REQUIRED)
@@ -40,6 +40,25 @@ public class AttachmentService {
     return new UploadAttachmentResponseDTO(attachmentEntity.getId());
   }
 
+  public AttachmentDTO getAttachment(Long attachmentId) throws AttachmentException {
+    // TODO check on user
+
+    assertAttachmentIdNotNull(attachmentId);
+    Optional<AttachmentEntity> attachmentOptional = attachmentRepository.findById(attachmentId);
+    assertAttachmentIsPresent(attachmentOptional);
+
+    PropertyMapper map = PropertyMapper.get();
+    AttachmentEntity attachmentEntity = attachmentOptional.get();
+    AttachmentDTO attachmentDto = new AttachmentDTO();
+
+    map.from(attachmentEntity::getFileName).to(attachmentDto::setFileName);
+    map.from(attachmentEntity::getContentType).to(attachmentDto::setContentType);
+    map.from(attachmentEntity::getSize).to(attachmentDto::setSize);
+    map.from(attachmentEntity::getBody).to(attachmentDto::setBody);
+
+    return attachmentDto;
+  }
+
   private void validateRequest(AttachmentDTO attachment) throws AttachmentException {
     String errorMessage = ValidatorUtils.validateRequestAndGetErrorMessage(attachment);
     if (errorMessage != null) {
@@ -47,11 +66,28 @@ public class AttachmentService {
     }
   }
 
-  public List<AttachmentEntity> checkIfAttachmentsExist(List<Long> attachmentIds) throws AttachmentException {
+  public List<AttachmentEntity> checkIfAttachmentsExist(List<Long> attachmentIds)
+      throws AttachmentException {
     List<AttachmentEntity> attachmentEntityList = attachmentRepository.findByIdIn(attachmentIds);
-    if(attachmentEntityList.isEmpty()){
-      throw new AttachmentException("Attachments not found", AttachmentException.Type.ENTITY_NOT_FOUND);
+    if (attachmentEntityList.isEmpty()) {
+      throw new AttachmentException(
+          "Attachments not found", AttachmentException.Type.ENTITY_NOT_FOUND);
     }
     return attachmentEntityList;
+  }
+
+  private void assertAttachmentIdNotNull(Long attachmentId) throws AttachmentException {
+    if (attachmentId == null) {
+      throw new AttachmentException(
+          "Null attachmentId", AttachmentException.Type.NULL_ATTACHMENT_ID);
+    }
+  }
+
+  private void assertAttachmentIsPresent(Optional<AttachmentEntity> attachment)
+      throws AttachmentException {
+    if (attachment.isEmpty()) {
+      throw new AttachmentException(
+          "No attachment found", AttachmentException.Type.ENTITY_NOT_FOUND);
+    }
   }
 }

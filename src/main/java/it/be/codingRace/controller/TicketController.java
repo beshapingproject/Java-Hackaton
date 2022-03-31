edd07519d1cd6d89ce99c4d0e7e7a4db05507b70
Giovanni Lamarmora <giovannilamarmora.working@gmail.com>
2022-03-31 09:29:10 +0200

    Removed CostumerID

--- src/main/java/it/be/codingRace/controller/TicketController.java
+++ src/main/java/it/be/codingRace/controller/TicketController.java
@@ -3,20 +3,19 @@ package it.be.codingRace.controller;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import it.be.codingRace.dto.TicketFilterDTO;
+import it.be.codingRace.dto.TicketRequestDTO;
+import it.be.codingRace.exception.TicketException;
+import it.be.codingRace.service.TicketService;
+import it.be.codingRace.utils.JsonResponseBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import it.be.codingRace.dto.TicketRequestDTO;
-import it.be.codingRace.exception.TicketException;
-import it.be.codingRace.service.TicketService;
-import it.be.codingRace.utils.JsonResponseBody;
-
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("ticket/{customerId}")
+@RequestMapping("ticket")
 public class TicketController {
 
   @Autowired private TicketService ticketService;
@@ -24,22 +23,24 @@ public class TicketController {
   @PostMapping("/list")
   @ApiOperation(value = "", notes = "GET TICKET LIST")
   public ResponseEntity<JsonResponseBody> getTicketList(
-      @ApiParam(name = "CUSTOMER", value = "Customer Id") @PathVariable Long customerId,
-      @ApiParam(name = "TICKET", value = "Ticket Body") @Valid @RequestBody TicketFilterDTO filters)
+            @ApiParam(name = "TICKET", value = "Ticket Body") @Valid @RequestBody TicketFilterDTO filters)
       throws TicketException {
     return ResponseEntity.status(HttpStatus.OK)
         .body(
             new JsonResponseBody(
-                HttpStatus.OK.value(), ticketService.getTicketList(customerId, filters)));
+                HttpStatus.OK.value(), ticketService.getTicketList(1L, filters)));
   }
 
   @PostMapping("/add")
   @ApiOperation(value = "", notes = "ADD NEW TICKET")
-  public ResponseEntity<JsonResponseBody> addTicket(@ApiParam(name = "CUSTOMER", value = "Customer Id") @PathVariable Long customerId,  @ApiParam(name = "TICKET", value = "Ticket Body") @Valid @RequestBody TicketRequestDTO ticket) throws TicketException {
-    return ResponseEntity.status(HttpStatus.OK).body(new JsonResponseBody(HttpStatus.OK.value(), ticketService.addTicket(customerId, ticket)));
+  public ResponseEntity<JsonResponseBody> addTicket(
+      @ApiParam(name = "TICKET", value = "Ticket Body") @Valid @RequestBody TicketRequestDTO ticket)
+      throws TicketException {
+    return ResponseEntity.status(HttpStatus.OK)
+        .body(new JsonResponseBody(HttpStatus.OK.value(), ticketService.addTicket(1L, ticket)));
   }
 
-  @PostMapping("/update")
+  @PostMapping("/update/{customerId}")
   @ApiOperation(value = "", notes = "UPDATE TICKET")
   public ResponseEntity<JsonResponseBody> updateTicket(
       @ApiParam(name = "CUSTOMER", value = "Customer Id") @PathVariable Long customerId,
@@ -50,5 +51,4 @@ public class TicketController {
             new JsonResponseBody(
                 HttpStatus.OK.value(), ticketService.updateTicket(customerId, ticket)));
   }
-
 }
